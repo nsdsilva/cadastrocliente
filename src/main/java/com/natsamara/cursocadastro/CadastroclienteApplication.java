cb32ced3b6a8a02e322f0ab0e8e493f0b603e17d
nsdsilva <natalisamaraa@gmail.com>
2021-08-24 21:13:42 -0300

    criando o projeto

---
+++ src/main/java/com/natsamara/cursocadastro/CadastroclienteApplication.java
@@ -0,0 +1,13 @@
+package com.natsamara.cursocadastro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroclienteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroclienteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/natsamara/cursocadastro/CadastroclienteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.natsamara.cursocadastro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroclienteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
